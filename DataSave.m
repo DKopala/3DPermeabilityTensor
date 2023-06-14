626913f5ad5779bf63ab487643225dccd3798ea1
DKopala <57114402+DKopala@users.noreply.github.com>
2023-06-14 15:45:37 +0200

    Added DataSave function to allow saving plots in .dat format

---
+++ DataSave.m
@@ -0,0 +1,25 @@
+% Save plots for angles to .dat separate files
+
+% data files naming
+baseFileName = 'saveData';
+
+% number of plots to be saved
+plotsNo = numel(g);
+
+% get H data
+plotDataH = tabH';
+
+for i = 1:plotsNo
+  % get g angle for a specific plot
+  currDeg = g(i);
+  % create file name based on the g angle
+  currFileName = strcat(baseFileName,int2str(currDeg),'.dat');
+  % get B data for specific g angle
+  plotDataB = tabB(i,:)';
+  plotData = [plotDataB, plotDataH];
+  % detete old data and save a new file
+  if isfile(currFileName)
+    delete(currFileName);
+  end
+  dlmwrite(currFileName,plotData,'delimiter','\t');
+end
